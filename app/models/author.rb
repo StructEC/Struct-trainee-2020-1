7629cd6659e1cd91f3b99f8b861b4aa3021a1060
PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>
2020-05-25 16:08:43 -0300

    Comandos explicação

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,5 +2,23 @@ class Author < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
 
-  has_many :books
+  has_many :books, dependent: :destroy
+
+  # Lista de comandos úteis
+  # Criação
+  # Author.create(name: 'Nome', year: 2020, email: 'e-mail')
+  #
+  # Update
+  # Author.update(name: 'Nome', year: 2020, email: 'e-mail')
+  #
+  # Query Method
+  # Author.all (retorna uma lista)
+  # Author.where(name: 'Nome', year: 2020) (retorna uma lista)
+  # Author.find_by(name: 'Nome') (retorna um objeto)
+  # Author.find(id) (retorna um objeto)
+  # object.books
+  #
+  # Deleção
+  # OBS: Lembrar do :dependent destroy no has_many
+  # object.destroy
 end
