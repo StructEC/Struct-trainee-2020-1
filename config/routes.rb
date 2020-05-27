1eeae959c3f92fe40f8ac8479a08e4e4f13890bb
Hugo-NF <hugonfonseca@hotmail.com>
2020-05-26 22:39:57 -0300

    :construction: Add routing examples

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#homepage'
+
+  # resources :books
+  # resources :books, except: [:destrou]
+  # get '/books', to: 'books#index'
+  #
+  scope 'books' do
+    get '/', to: 'books#index'
+  end
+
+  # namespace 'books' do
+  #   get '/', to: 'books#index'
+  # end
+
 end
